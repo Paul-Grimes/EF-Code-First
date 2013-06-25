5c75e7471f321e5602dcb7f4ce318ede84cc9fe2
Paul Grimes <paul.grimes@byprime.com>
2013-06-25 14:39:36 -0400

    Initial Commit

---
+++ Model/Lodging.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Lodging
+    {
+        public int LodgingId { get; set; }
+
+        public string name { get; set; }
+        public string owner { get; set; }
+        public bool isResort { get; set; }
+
+        public Destination destination { get; set; }
+    }
+}
