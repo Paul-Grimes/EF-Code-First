bf84cf86121d3099f0a190f2567b2f636114f52a
Paul Grimes <paul.grimes@byprime.com>
2013-06-26 17:36:55 -0400

    Change to public class

--- DataAccess/BreakAwayContext.cs
+++ DataAccess/BreakAwayContext.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.Entity;
+using Model;
 
 namespace DataAccess
 {
-    class BreakAwayContext
+    public class BreakAwayContext : DbContext 
     {
+        public DbSet<Destination> Destinations { get; set; }
+        public DbSet<Lodging> Lodgings { get; set; }
     }
 }
