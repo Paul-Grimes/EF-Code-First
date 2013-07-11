e1f86120e010bb8477ff2e4f2e313b6134a296aa
Paul Grimes <paul.grimes@byprime.com>
2013-07-11 09:49:34 -0400

    Added data annotations

--- Model/Destination.cs
+++ Model/Destination.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace Model
 {
@@ -10,10 +13,13 @@ namespace Model
     {
 
         public int DestinationId { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Country { get; set; }
+        [MaxLength(500)]
         public string Description { get; set; }
 
+        [Column(TypeName="image")]
         public byte[] photo { get; set; }
 
         public List<Lodging> Lodgings { get; set; }
