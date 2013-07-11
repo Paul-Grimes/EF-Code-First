8bcd9454e156d3a3de23199704ce2fe24cee0c6c
Paul Grimes <paul.grimes@byprime.com>
2013-07-11 09:50:01 -0400

    Added data anotations

--- Model/Lodging.cs
+++ Model/Lodging.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace Model
 {
@@ -10,8 +13,11 @@ namespace Model
     {
         public int LodgingId { get; set; }
 
-        public string name { get; set; }
-        public string owner { get; set; }
+        [Required]
+        [MaxLength(200)]
+        [MinLength(10)]
+        public string Name { get; set; }
+        public string Owner { get; set; }
         public bool isResort { get; set; }
 
         public Destination destination { get; set; }
