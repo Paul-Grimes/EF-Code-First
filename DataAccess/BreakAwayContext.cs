4a5fe446bf379a74e3f4ac096616435dbc1eee91
Paul Grimes <paul.grimes@byprime.com>
2013-07-12 16:04:53 -0400

    Added OnModelCreating function
    This is an alternative to doint data annotations.
    
    This will also start a divergent branch.

--- DataAccess/BreakAwayContext.cs
+++ DataAccess/BreakAwayContext.cs
@@ -7,5 +7,18 @@ namespace DataAccess
     {
         public DbSet<Destination> Destinations { get; set; }
         public DbSet<Lodging> Lodgings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Destination>()
+                .Property(d => d.Name).IsRequired();
+            modelBuilder.Entity<Destination>()
+                .Property(d => d.Description).HasMaxLength(500);
+            modelBuilder.Entity<Destination>()
+                .Property(d => d.photo).HasColumnType("image");
+
+            modelBuilder.Entity<Lodging>()
+                .Property(l => l.name).IsRequired().HasMaxLength(200);
+        }
     }
 }
